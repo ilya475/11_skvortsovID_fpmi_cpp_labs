d11d3607c320f90905902610be71735cfd9fcd1e
ilya475 <cccc.motorshow@gmail.com>
2025-09-09 09:52:35 +0300

    Add files via upload

---
+++ semester_1/lab1_introduction/lab1_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	std::cout << "Введите нулевой член прогрессии, верхнюю границу прогрессии, разность прогрессии\n";
+	int a_0, n, d;
+	std::cin >> a_0 >> n >> d;
+	for (int i = a_0; i <= n; i += d)
+	{
+		if (i % 3 == 0)
+			std::cout << "Член прогресcии кратный трём: " << i << std::endl;
+	}
+
+	return 0;
+}
+
+
+
---
+++ semester_1/lab1_introduction/lab1_10.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int n, mod_1, mod_2, sum_1, sum_2;
+	sum_1 = 0;
+	sum_2 = 0;
+	std::cout << "Введите шестизначное число" << std::endl;
+	std::cin >> n;
+	for (int i = 1; i <= 3; i++) {
+		mod_1 = n % 10;
+		n = n / 10;
+		sum_1 += mod_1;
+	}
+	for (int j = 1; j <= 3; j++) {
+		mod_2 = n % 10;
+		n = n / 10;
+		sum_2 += mod_2;
+	}
+	if (sum_1 == sum_2)
+		std::cout << "Это счастливое число";
+	else 
+		std::cout << "Это число не является счастливым";
+	return 0;
+}
