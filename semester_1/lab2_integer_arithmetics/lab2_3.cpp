b6bc7f1880bb30bb31c3601bca5db4aabf344501
ilya475 <cccc.motorshow@gmail.com>
2025-09-16 22:50:00 +0300

    Add files via upload

---
+++ semester_1/lab2_integer_arithmetics/lab2_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    std::cout << "Введите натуральное число: ";
+    int num_n, n, mod;
+    std::cin >> n;
+    int min_num = 9; //начинаем с максимальной цифры
+    if (!std::cin >> n || n<=0) {
+        std::cout << "Error";
+    std::exit(1);
+    }
+    num_n = n; // в num_n мы записываем число, равное n
+    while (n > 0) {
+        mod = n % 10;
+        if (mod !=0 && mod < min_num)
+            min_num = mod;
+        n /= 10;
+
+  }
+    
+    std::cout << "Новое число: " << min_num << num_n << min_num;
+    return 0;
+}
