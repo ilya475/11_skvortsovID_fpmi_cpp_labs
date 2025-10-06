2865eb706cade042e42bca2ad6f3a4ef37c90c0d
ilya475 <cccc.motorshow@gmail.com>
2025-10-07 01:28:15 +0300

    Update lab3_static.cpp

--- semester_1/lab3_pointers_and_arrays/lab3_static.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_static.cpp
@@ -3,17 +3,63 @@
 #include<random>
 #include<cstdlib>
 const int all_el = 1000;
-void keyboard() {
-	double mas[all_el];
-	double product = 1;
-	int i = 0;
-	int counter = 0;
-	std::cout << "Enter a number of elements in your array: ";
-	int number_of_elements;
-	if (!(std::cin >> number_of_elements) || number_of_elements > all_el || number_of_elements <= 0) {
-		std::cout << "Error";
-		std::exit(1);
+int i = 0;
+int counter = 0;
+double product = 1;
+void acts_with_mas(double mas[], int number_of_elements) {
+	std::cout << "Your mas is: ";
+	for (i = 0; i < number_of_elements; i++) {
+		std::cout << mas[i] << " ";
 	}
+	std::cout << std::endl;
+	for (i = 0; i < number_of_elements; i++) {
+		bool different = true;
+		for (int j = 0; j < i; j++) {
+			if (mas[i] == mas[j]) {
+				different = false;
+				break;
+			}
+		}
+		if (different)
+			counter++;
+	}
+	std::cout << "The number of different elements is: " << counter << std::endl;
+	double first_element = mas[0];
+	for (int i = 0; i < number_of_elements; i++) {
+		if (std::abs(first_element) < std::abs(mas[i])) {
+			first_element = mas[i];
+		}
+	}
+	for (int i = 0; i < number_of_elements; i++) {
+		if (mas[i] == first_element) {
+			if (i == number_of_elements - 1) {
+				std::cout << "Max element is last, thats why product is 1\n";
+			}
+			for (int j = i + 1; j < number_of_elements; j++) {
+				product = product * mas[j];
+			}
+			break;
+		}
+	}
+	std::cout << "The product of elements after the abs_max is :" << product << std::endl;
+	if (product == 1) {
+		std::cout << "Max element is last, thats why product is 1\n";
+	}
+	for (int j = 0; j < number_of_elements - 1; j++) {
+		for (i = 0; i < number_of_elements - 1; i++) {
+			if (mas[i] > mas[i + 1]) {
+				double temp = mas[i];
+				mas[i] = mas[i + 1];
+				mas[i + 1] = temp;
+			}
+		}
+	}
+	std::cout << "Sorted mas is: \n";
+	for (i = 0; i < number_of_elements; i++) {
+		std::cout << mas[i] << std::endl;
+	}
+}
+void keyboard(double mas[], int number_of_elements) {
 	std::cout << "Enter elements mas in your arr: \n";
 	for (; i < number_of_elements; i++) {
 		if (!(std::cin >> mas[i])) {
@@ -21,66 +67,15 @@ void keyboard() {
 			std::exit(1);
 		}
 	}
-    for (i = 0; i < number_of_elements; i++){
-		bool different = true;
-		for (int j = 0; j < i; j++) {
-			if (mas[i] == mas[j]) {
-				different = false;
-				break;
-			}
-		}
-		if (different)
-			counter++;
-	}
-	std::cout << "The number of different elements is: " << counter << std::endl;
-	double first_element = mas[0];
-	for (i = 0; i < number_of_elements; i++) {
-		if (abs(first_element) < abs(mas[i])) {
-			first_element = mas[i];
-		}
-	}
-	for (i = 0; i < number_of_elements; i++) {
-		if (mas[i] == first_element) {
-			if (i == number_of_elements - 1) {
-				std::cout << "Max element is last, thats why product is 1\n";	
-			}
-			for (int j = i + 1; j < number_of_elements; j++) {
-				product = product * mas[j];
-			}
-		}
-	}
-    std::cout << "The product of elements after the abs_max is :" << product << std::endl;
-	for (int j = 0; j < number_of_elements - 1; j++) {
-		for (i = 0; i < number_of_elements - 1; i++) {
-			if (mas[i] > mas[i + 1]) {
-				double temp = mas[i];
-				mas[i] = mas[i + 1];
-				mas[i + 1] = temp;
-			}
-		}
-	}
-	std::cout << "Sorted mas is: \n";
-	for (i = 0; i < number_of_elements; i++) {
-		std::cout << mas[i] << std::endl;
-	}
+	acts_with_mas(mas, number_of_elements);
 }
-void random_enter() {
+void random_enter(double mas[], int number_of_elements) {
 	std::cout << "Enter of random interval(the first number is start, the second is finish): ";
 	double start, finish;
 	if (!(std::cin >> start >> finish)) {
 		std::cout << "Error";
 		std::exit(1);
 	}
-    int number_of_elements;
-	std::cout << "Enter a number of elements in your array: ";
-	if (!(std::cin >> number_of_elements) || number_of_elements > all_el || number_of_elements <= 0) {
-		std::cout << "Error";
-		std::exit(1);
-	}
-	double mas[all_el];
-	double product = 1;
-	int i = 0;
-	int counter = 0;
 	std::mt19937 gen(45218965);
 	double change_if = start + finish;
 	if (start > finish) {
@@ -88,78 +83,32 @@ void random_enter() {
 		finish = change_if - start;
 	}
 	std::uniform_real_distribution<double> dist(start, finish);
-	std::cout << "mas is: " << std::endl;
 	for (i = 0; i < number_of_elements; i++) {
 		double random_element = dist(gen);
 		mas[i] = random_element;
-		std::cout << mas[i] << std::endl;
 	}
-	for (int j = 0; j < number_of_elements - 1; j++) {
-		for (i = 0; i < number_of_elements - 1; i++) {
-			if (mas[i] > mas[i + 1]) {
-				double temp = mas[i];
-				mas[i] = mas[i + 1];
-				mas[i + 1] = temp;
-			}
-		}
-	}
-    for (i = 0; i < number_of_elements; i++) {
-		bool different = true;
-		for (int j = 0; j < i; j++) {
-			if (mas[i] == mas[j]) {
-				different = false;
-				break;
-			}
-		}
-		if (different)
-			counter++;
-	}
-	std::cout << "The number of different elements is: " << counter << std::endl;
-	double first_element = mas[0];
-	for (i = 0; i < number_of_elements; i++) {
-		if (abs(first_element) < abs(mas[i])) {
-			first_element = mas[i];
-		}
-	}
-	for (i = 0; i < number_of_elements; i++) {
-		if (mas[i] == first_element) {
-			if (i == number_of_elements - 1) {
-				std::cout << "Max element is last, thats why product is 1\n";	
-			}
-			for (int j = i + 1; j < number_of_elements; j++) {
-				product = product * mas[j];
-			}
-		}
-	}
-    std::cout << "The product of elements after the  abs_max is :" << product << std::endl;
-	for (int j = 0; j < number_of_elements - 1; j++) {
-		for (i = 0; i < number_of_elements - 1; i++) {
-			if (mas[i] > mas[i + 1]) {
-				double temp = mas[i];
-				mas[i] = mas[i + 1];
-				mas[i + 1] = temp;
-			}
-		}
-	}
-	std::cout << "Sorted mas is: \n";
-	for (i = 0; i < number_of_elements; i++) {
-		std::cout << mas[i] << std::endl;
-	}
+	acts_with_mas(mas, number_of_elements);
 }
 int main()
 {
+	double mas[all_el];
+	int number_of_elements;
 	std::cout << "Enter 0 if you want to enter elements manually or other number if random enter: ";
 	double variants;
 	if (!(std::cin >> variants)) {
 		std::cout << "Try again";
 		return 1;
 	}
+	std::cout << "Enter a number of elements in your array: ";
+	if (!(std::cin >> number_of_elements) || number_of_elements > all_el || number_of_elements <= 0) {
+		std::cout << "Error";
+		std::exit(1);
+	}
 	if (!variants) {
-		keyboard();
+		keyboard(mas, number_of_elements);
 	}
 	else {
-		random_enter();
+		random_enter(mas, number_of_elements);
 	}
-    return 0;
+	return 0;
 }
-
