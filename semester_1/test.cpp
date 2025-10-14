3edd954f9fc790a5e5a0dae4aba5a18a3bbc5442
ilya475 <cccc.motorshow@gmail.com>
2025-10-14 10:22:25 +0300

    Add files via upload

---
+++ semester_1/test.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+const int N = 1000;
+void bubblesort(int* mas, int number_of_elements) {
+    for (int i = 0; i < number_of_elements - 1; i++) {
+        for (int j = 0; j < number_of_elements - 1; j++) {
+            if (abs(mas[j]) > abs(mas[j + 1])) {
+                int temp = mas[j];
+                mas[j] = mas[j + 1];
+                mas[j + 1] = temp;
+            }
+        }
+    }
+}
+void printmas(int* mas, int number_ofelements) {
+    for (int i = 0; i < N; i++) {
+        std::cout << mas[i] << " ";
+    }
+}
+int main()
+{
+    int number_of_elements;
+    int mas[N];
+    std::cout << "Enter number of elements: ";
+    std::cin >> number_of_elements;
+    for (int i = 0; i < number_of_elements; i++) {
+        std::cin >> mas[i];
+    }
+    bubblesort(mas, number_of_elements);
+    printmas(mas, number_of_elements);
+    return 0;
+}
+
