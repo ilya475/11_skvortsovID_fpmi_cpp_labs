ec5c1828d20a18d88a804f1cf21470515585c493
ilya475 <cccc.motorshow@gmail.com>
2025-11-10 23:21:49 +0300

    Update lab_5_4.cpp

--- semester_1/lab5_strings/lab_5_4.cpp
+++ semester_1/lab5_strings/lab_5_4.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include <algorithm>
 #include <cmath>
@@ -6,8 +6,8 @@ bool PrimeNumbers(int number_from_string) {
 	if (number_from_string < 2) {
 		return false;
 	}
-	int p = sqrt(number_from_string);
-	for (int i = 2; i < p; i++) {
+	int limit = sqrt(number_from_string);
+	for (int i = 2; i <= limit; i++) {
 		if (number_from_string % i == 0) {
 			return false;
 		}
@@ -18,45 +18,55 @@ int main()
 {
 	try {
 		int number;
-		std::string s1{};
-		std::string s2{};
-		std::string temp_symbol{};
+		std::string enter_string;
+		std::string result_string;
+		std::string temp_symbol;
 		std::cout << "Enter string: " << std::endl;
-		std::getline(std::cin, s1);
-		for (size_t i = 0; i <= s1.size(); i++) {
-			char one_symbol = s1[i];
-			if (std::isdigit(one_symbol)) {
-				temp_symbol += one_symbol;
+		std::getline(std::cin, enter_string);
+		for (size_t i = 0; i < enter_string.size(); i++) {
+			if (std::isdigit(enter_string[i])) {
+				temp_symbol += enter_string[i];
 			}
 			else {
 				if (!(temp_symbol.empty())) {
-					if (temp_symbol.size() > 9) {
-						throw "Too large number";
+					temp_symbol.erase(0, temp_symbol.find_first_not_of('0'));
+					if (temp_symbol.empty()) {
+						temp_symbol = "0";
+					}
+					if (temp_symbol.size() > 10 || (temp_symbol.size() == 10 && temp_symbol > "2147483647")) {
+						throw "There is too large number in your string";
 					}
 					int number = std::stoi(temp_symbol);
 					if (PrimeNumbers(number)) {
-						if (!(s2.empty())) {
-							s2 += temp_symbol + " ";
+						if (!(result_string.empty())) {
+							result_string += " ";
 						}
-						s2 += temp_symbol;
+						result_string += temp_symbol;
 					}
 					temp_symbol.clear();
 				}
 			}
 		}
 		if (!(temp_symbol.empty())) {
+			temp_symbol.erase(0, temp_symbol.find_first_not_of('0'));
+			if (temp_symbol.empty()) {
+				temp_symbol = "0";
+			}
+			if (temp_symbol.size() > 10 || (temp_symbol.size() == 10 && temp_symbol > "2147483647")) {
+				throw "There is too large number in your string";
+			}
 			int number = std::stoi(temp_symbol);
 			if (PrimeNumbers(number)) {
-				if (!s2.empty()) {
-					s2 += temp_symbol + " ";
+				if (!result_string.empty()) {
+					result_string += " ";
 				}
-				s2 += temp_symbol;
+				result_string += temp_symbol;
 			}
 		}
-		if (s2.empty() == true) {
+		if (result_string.empty() == true) {
 			throw "There are no prime numbers in your string, try again";
 		}
-		std::cout << s2;
+		std::cout << "Your new string with prime numbers: " << result_string << std::endl;
 		return 0;
 	}
 	catch (const char* msg) {
