fb737b11d176486f9af35652de3302933cbcc494
ilya475 <cccc.motorshow@gmail.com>
2025-12-16 15:46:58 +0300

    Update lab6_3.cpp(better variant without vector)

--- semester_1/lab6_files/lab6_3.cpp
+++ semester_1/lab6_files/lab6_3.cpp
@@ -1,85 +1,80 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
 #include <sstream>
 #include <cctype>
 bool IsEmptyInputFile(std::ifstream& in);
 int CountWordsInLine(const std::string& line);
-void ReadData(const std::string& file_name, std::vector<std::string>& lines, std::vector<size_t>& count_words_in_line);
-int MaxAmountOfWordsInLine(std::vector<size_t>& count_words_in_line);
-void SaveBiggerLines(std::vector<size_t>& count_words_in_line, std::vector<std::string>& lines, const std::string& output_file);
+int MaxAmountOfWordsInLine(const std::string& file_name);
+void SaveBiggerLines(const std::string& input_file, const std::string& output_file, int max_words);
+
 bool IsEmptyInputFile(std::ifstream& in) {
-	return (in.peek() == EOF);
-}
-void ReadData(const std::string& file_name, std::vector<std::string>& lines, std::vector<size_t>& count_words_in_line) {
-	std::ifstream in(file_name);
-	if (!in.is_open()) {
-		throw ("Can not open " + file_name);
-	}
-	if (IsEmptyInputFile(in)) {
-		in.close();
-		throw (file_name + " is empty");
-	}
-	std::string line;
-	while (getline(in, line)) {
-		lines.push_back(line);
-		count_words_in_line.push_back(CountWordsInLine(line));
-	}
-	in.close();
+    return in.peek() == EOF;
 }
 int CountWordsInLine(const std::string& line) {
-	int words_in_each_line = 0;
-	std::istringstream iss(line);
-	std::string word;
-	while (iss >> word) {
-		++words_in_each_line;
-	}
-	return words_in_each_line;
+    int words_in_each_line = 0;
+    std::istringstream iss(line);
+    std::string word;
+    while (iss >> word) {
+        ++words_in_each_line;
+    }
+    return words_in_each_line;
 }
-int MaxAmountOfWordsInLine(std::vector<size_t>& count_words_in_line) {
-	int max = count_words_in_line[0];
-	for (size_t i = 0; i < count_words_in_line.size(); ++i) {
-		if (count_words_in_line[i] > max) {
-			max = count_words_in_line[i];
-		}
-	}
-	return max;
+int MaxAmountOfWordsInLine(const std::string& file_name) {
+    std::ifstream in(file_name);
+    if (!in.is_open()) {
+        throw ("Can not open " + file_name);
+    }
+    if (IsEmptyInputFile(in)) {
+        throw (file_name + " is empty");
+    }
+    std::string line;
+    int max_words = 0;
+    while (std::getline(in, line)) {
+        int words = CountWordsInLine(line);
+        if (words > max_words) {
+            max_words = words;
+        }
+    }
+    return max_words;
 }
-void SaveBiggerLines(std::vector<size_t>& count_words_in_line, std::vector<std::string>& lines, const std::string& output_file) {
-	std::ofstream out("output.txt");
-	if (!out.is_open()) {
-		throw ("Can not open + " + output_file);
-	}
-	size_t printed_already_lines = 0;
-	for (size_t i = 0; i < count_words_in_line.size(); ++i) {
-		if (count_words_in_line[i] == MaxAmountOfWordsInLine(count_words_in_line)) {
-				if (printed_already_lines < 10) {
-					out << lines[i] << std::endl;
-					++printed_already_lines;
-				}
-		}	
-	}
-	out.close();
+void SaveBiggerLines(const std::string& input_file, const std::string& output_file, int max_words) {
+    std::ifstream in(input_file);
+    if (!in.is_open()) {
+        in.close();
+        throw ("Cannot open " + input_file);
+    }
+    std::ofstream out(output_file);
+    if (!out.is_open()) {
+        out.close();
+        throw ("Cannot open " + output_file);
+    }
+    std::string line;
+    size_t already_printed = 0;
+    while (std::getline(in, line) && already_printed < 10) {
+        if (CountWordsInLine(line) == max_words) {
+            out << line << std::endl;
+            ++already_printed;
+        }
+    }
+    in.close();
+    out.close();
 }
-int main()
-{
-	try {
-		const std::string input_file{"input.txt"};
-		const std::string output_file{ "otput.txt" };
-		std::vector<std::string> lines;
-		std::vector<size_t> count_words_in_line;
-		ReadData(input_file, lines, count_words_in_line);
-		SaveBiggerLines(count_words_in_line, lines, output_file);
-		std::cout << output_file + " is done" << std::endl;
-		return 0;
-	}
-	catch (std::string& msg) {
-		std::cerr << msg;
-		return 1;
-	}
-	catch (...) {
-		std::cerr << "Unknown error";
-		return 1;
-	}
+int main() {
+    try {
+        const std::string input_file{ "input.txt" };
+        const std::string output_file{ "output.txt" };
+        int max_words = MaxAmountOfWordsInLine(input_file);
+        SaveBiggerLines(input_file, output_file, max_words);
+        std::cout << output_file + " is done" << std::endl;
+        return 0;
+    }
+    catch (std::string& msg) {
+        std::cerr << msg << std::endl;
+        return 1;
+    }
+    catch (...) {
+        std::cerr << "Unknown error" << std::endl;
+        return 1;
+    }
 }
