3215bed13280c9942e3a0848246601f383b2fb15
ilya475 <cccc.motorshow@gmail.com>
2025-12-23 13:42:30 +0300

    Update main.cpp

--- semester_1/rgr3_set_list_stl/part1/main.cpp
+++ semester_1/rgr3_set_list_stl/part1/main.cpp
@@ -1,9 +1,40 @@
-
-
-
-int main() {
-
-    // your code goes here
-
-    return 0;
+#include <iostream>
+#include <set>
+#include <cmath>
+int main()
+{
+	try {
+		int number;
+		std::cout << "Enter number you want to see primes before: ";
+		if (!(std::cin >> number) || number < 2) {
+			throw "Invalid input of entering number";
+		}
+		int bord = sqrt(number);
+		std::set<int> numbers;
+		for (int i = 2; i <= number; ++i) {
+			numbers.insert(i);
+		}
+		for (int i = 2; i <= bord; ++i) {
+			if (numbers.find(i) != numbers.end()) {
+				for (int j = i * i; j <= number; j += i) {
+					numbers.erase(j);
+				}
+			}
+		}
+		std::cout << "Primes before " << number << std::endl;
+		for (int prime : numbers)
+		{
+			std::cout << prime << " ";
+		}
+		std::cout << std::endl;
+		return 0;
+	}
+	catch (const char* msg) {
+		std::cerr << msg;
+		return 1;
+	}
+	catch (...) {
+		std::cerr << "Unknown error";
+		return 1;
+	}
 }
